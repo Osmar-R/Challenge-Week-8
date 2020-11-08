1121751511a38e9e20b9d809b2980f0825ad6215
Osmar Rodriguez <osmar.vasquez7@gmail.com>
2020-11-08 16:18:02 -0500

    first commit

---
+++ Easy/easy.js
@@ -0,0 +1,5 @@
+const firstName = ["Bob", "Jimmy", "Maria"];
+const lastName = ["Evans", "Brown", "Sanchez"];
+const age = ["18", "21", "25"];
+
+console.log(`Hello my name is ${firstName[1]} ${lastName[1]} and im ${age[1]} years old`)
